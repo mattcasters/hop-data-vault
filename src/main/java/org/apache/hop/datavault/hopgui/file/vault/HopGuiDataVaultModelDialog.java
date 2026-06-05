2440486ccc177e25f9cd84f0f4abfc33fe0f5e39
mattcasters <mattcasters@gmail.com>
2026-06-05 10:07:54 +0200

    Model dialog added

---
+++ src/main/java/org/apache/hop/datavault/hopgui/file/vault/HopGuiDataVaultModelDialog.java
@@ -0,0 +1,182 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.hop.datavault.hopgui.file.vault;
+
+import org.apache.hop.core.Const;
+import org.apache.hop.core.exception.HopException;
+import org.apache.hop.core.variables.IVariables;
+import org.apache.hop.datavault.metadata.DataVaultConfiguration;
+import org.apache.hop.datavault.metadata.DataVaultModel;
+import org.apache.hop.i18n.BaseMessages;
+import org.apache.hop.ui.core.PropsUi;
+import org.apache.hop.ui.core.dialog.BaseDialog;
+import org.apache.hop.ui.core.widget.MetaSelectionLine;
+import org.apache.hop.ui.hopgui.HopGui;
+import org.apache.hop.ui.pipeline.transform.BaseTransformDialog;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.FormAttachment;
+import org.eclipse.swt.layout.FormData;
+import org.eclipse.swt.layout.FormLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+
+/**
+ * Dialog to edit the properties of a DataVaultModel (description, default configuration etc).
+ */
+public class HopGuiDataVaultModelDialog {
+  private static final Class<?> PKG = HopGuiDataVaultModelDialog.class;
+
+  private Shell parent;
+  private HopGui hopGui;
+  private IVariables variables;
+  private DataVaultModel input;
+  private Shell shell;
+
+  // Widgets
+  private Text wName;
+  private Text wDescription;
+  private MetaSelectionLine<DataVaultConfiguration> wConfigurationName;
+
+  private boolean ok;
+
+  public HopGuiDataVaultModelDialog(Shell parent, HopGui hopGui, DataVaultModel model) {
+    this.parent = parent;
+    this.hopGui = hopGui;
+    this.variables = hopGui.getVariables();
+    this.input = model;
+  }
+
+  public boolean open() {
+    shell = new Shell(parent, BaseDialog.getDefaultDialogStyle());
+    PropsUi.setLook(shell);
+    shell.setText(BaseMessages.getString(PKG, "HopGuiDataVaultModelDialog.Title", input.getName()));
+
+    FormLayout formLayout = new FormLayout();
+    formLayout.marginWidth = PropsUi.getFormMargin();
+    formLayout.marginHeight = PropsUi.getFormMargin();
+    shell.setLayout(formLayout);
+
+    int margin = PropsUi.getMargin();
+    int middle = 30;
+
+    // Buttons at the bottom (using standard positioning)
+    Button wOk = new Button(shell, SWT.PUSH);
+    wOk.setText(BaseMessages.getString(PKG, "System.Button.OK"));
+    wOk.addListener(SWT.Selection, e -> ok());
+    Button wCancel = new Button(shell, SWT.PUSH);
+    wCancel.setText(BaseMessages.getString(PKG, "System.Button.Cancel"));
+    wCancel.addListener(SWT.Selection, e -> cancel());
+
+    BaseTransformDialog.positionBottomButtons(shell, new Button[] {wOk, wCancel}, margin, null);
+
+    // Name
+    Label wlName = new Label(shell, SWT.RIGHT);
+    wlName.setText(BaseMessages.getString(PKG, "HopGuiDataVaultModelDialog.Name.Label"));
+    PropsUi.setLook(wlName);
+    FormData fdlName = new FormData();
+    fdlName.left = new FormAttachment(0, 0);
+    fdlName.top = new FormAttachment(0, margin);
+    fdlName.right = new FormAttachment(middle, -margin);
+    wlName.setLayoutData(fdlName);
+
+    wName = new Text(shell, SWT.SINGLE | SWT.LEFT | SWT.BORDER);
+    PropsUi.setLook(wName);
+    FormData fdName = new FormData();
+    fdName.left = new FormAttachment(middle, 0);
+    fdName.top = new FormAttachment(0, margin);
+    fdName.right = new FormAttachment(100, 0);
+    wName.setLayoutData(fdName);
+    wName.addModifyListener(e -> input.setChanged());
+
+    // Description
+    Label wlDescription = new Label(shell, SWT.RIGHT);
+    wlDescription.setText(BaseMessages.getString(PKG, "HopGuiDataVaultModelDialog.Description.Label"));
+    PropsUi.setLook(wlDescription);
+    FormData fdlDescription = new FormData();
+    fdlDescription.left = new FormAttachment(0, 0);
+    fdlDescription.top = new FormAttachment(wName, margin);
+    fdlDescription.right = new FormAttachment(middle, -margin);
+    wlDescription.setLayoutData(fdlDescription);
+
+    wDescription = new Text(shell, SWT.SINGLE | SWT.LEFT | SWT.BORDER);
+    PropsUi.setLook(wDescription);
+    FormData fdDescription = new FormData();
+    fdDescription.left = new FormAttachment(middle, 0);
+    fdDescription.top = new FormAttachment(wName, margin);
+    fdDescription.right = new FormAttachment(100, 0);
+    wDescription.setLayoutData(fdDescription);
+    wDescription.addModifyListener(e -> input.setChanged());
+
+    // Configuration name (default for the model) using MetaSelectionLine for metadata selection
+    wConfigurationName =
+        new MetaSelectionLine<>(
+            variables,
+            hopGui.getMetadataProvider(),
+            DataVaultConfiguration.class,
+            shell,
+            SWT.SINGLE | SWT.LEFT | SWT.BORDER,
+            BaseMessages.getString(PKG, "HopGuiDataVaultModelDialog.ConfigurationName.Label"),
+            BaseMessages.getString(PKG, "HopGuiDataVaultModelDialog.ConfigurationName.ToolTip"));
+    FormData fdConfigurationName = new FormData();
+    fdConfigurationName.left = new FormAttachment(0, 0);
+    fdConfigurationName.top = new FormAttachment(wDescription, margin);
+    fdConfigurationName.right = new FormAttachment(100, 0);
+    wConfigurationName.setLayoutData(fdConfigurationName);
+    wConfigurationName.addModifyListener(e -> input.setChanged());
+
+    getData();
+
+    BaseDialog.defaultShellHandling(shell, e -> ok(), e -> cancel());
+
+    return ok;
+  }
+
+  private void getData() {
+    if (input.getName() != null) wName.setText(input.getName());
+    if (input.getDescription() != null) wDescription.setText(input.getDescription());
+    try {
+      wConfigurationName.fillItems();
+      wConfigurationName.setText(Const.NVL(input.getConfigurationName(), ""));
+    } catch (HopException e) {
+      // ignore for this simple dialog; just set what we have
+      wConfigurationName.setText(Const.NVL(input.getConfigurationName(), ""));
+    }
+  }
+
+  private void ok() {
+    input.setName(wName.getText());
+    input.setDescription(wDescription.getText());
+    input.setConfigurationName(wConfigurationName.getText());
+
+    ok = true;
+    dispose();
+  }
+
+  private void cancel() {
+    ok = false;
+    dispose();
+  }
+
+  private void dispose() {
+    if (!shell.isDisposed()) {
+      shell.dispose();
+    }
+  }
+}
